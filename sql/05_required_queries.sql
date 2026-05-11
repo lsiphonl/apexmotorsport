8e724caf70dc067f199d936e4b633c63d9fab998
marco <marcogoncalves2808@gmail.com>
2026-05-11 21:25:05 +0200

    Queries was added.

--- sql/05_required_queries.sql
+++ sql/05_required_queries.sql
@@ -195,3 +195,89 @@ WHERE Event_Date = (
     SELECT MIN(Event_Date)
     FROM EVENT
 );
+
+-- Show car with power unit with mileage less than 2500 and cureently fitted (Join)
+SELECT
+    pu.Part_Num,
+    pu.Mileage,
+    c.Car_ID
+FROM POWER_UNIT pu
+JOIN CAR_PART cp
+ON pu.Part_Num = cp.Part_Num
+JOIN CAR c
+ON cp.Car_ID = c.Car_ID
+WHERE pu.Mileage < 2500;
+
+-- Display Car_Id and FIA_Car_Num where the total number of rows are less than or equal to 5 (Limit Rows/Columns)
+SELECT Car_ID, FIA_Car_Num
+FROM CAR
+WHERE ROWNUM <= 5;
+
+-- All power units along with their mileage in Descending order (Sorting)
+SELECT *
+FROM POWER_UNIT
+ORDER BY Mileage DESC;
+
+-- Total number of parts per part type (Group By/ Having)
+SELECT
+    Part_Type,
+    COUNT(*) AS Total_Parts
+FROM PART
+GROUP BY Part_Type
+HAVING COUNT(*) > 1;
+
+-- Chassis that passed the crash test as well as being made of Carbon Fibre (LIKE/AND/OR)
+SELECT Part_Num, Crash_Test_Status, Monocoque_Material, Weight
+FROM CHASSIS
+WHERE Crash_Test_Status = 'Passed'
+  AND Monocoque_Material = 'CarbonFibre';
+  
+SELECT Part_Num, Aero_Component_Type, Specification_Version
+FROM AERO_COMPONENT
+WHERE Specification_Version LIKE 'v2%';
+
+-- Aero Components displayed in lower case with its version (Variable/Character Functions)
+SELECT Part_Num,
+       LOWER(Aero_Component_Type) AS Component_Type_Lower,
+       Specification_Version
+FROM AERO_COMPONENT;
+
+-- Gearbox mileage per race event (Round/Trunc)
+SELECT Part_Num, Mileage, Race_Events_Used,
+       CASE
+           WHEN Race_Events_Used > 0
+           THEN ROUND(Mileage / Race_Events_Used, 2)
+           ELSE 0
+       END AS Avg_KM_Per_Event
+FROM GEARBOX;
+
+-- How many days was a removed part was fitted before it was removed (Date Function)
+SELECT Car_ID, Part_Num,
+       Fitment_Date,
+       Removal_Date,
+       (Removal_Date - Fitment_Date)           AS Days_In_Use
+FROM CAR_PART
+WHERE Fitment_Status = 'Removed'
+  AND Removal_Date IS NOT NULL;
+
+-- Count the total number of parts in inventory (Aggreagte Functions)
+SELECT COUNT(*) AS Total_Parts
+FROM PART;
+
+-- Cars with their current fitted parts (Inner Join)
+SELECT c.Car_ID, c.FIA_Car_Num, c.Car_Status,
+       p.Part_Num, p.Part_Type, p.Part_Condition,
+       cp.Fitment_Date
+FROM CAR c
+INNER JOIN CAR_PART cp ON c.Car_ID    = cp.Car_ID
+INNER JOIN PART p      ON cp.Part_Num = p.Part_Num
+WHERE cp.Fitment_Status = 'Fitted';
+
+-- Cars that have atleast one part fitted to it (Sub-Query)
+SELECT Car_ID, FIA_Car_Num, Car_Status
+FROM CAR
+WHERE Car_ID IN (
+    SELECT DISTINCT Car_ID
+    FROM CAR_PART
+    WHERE Fitment_Status = 'Fitted'
+);
